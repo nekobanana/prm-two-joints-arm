eaa029fc5f61448bee5b20c2082a142446b173ee
nekobanana <pru98@hotmail.it>
2024-02-26 17:04:13 +0100

    Double point in C_space

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from matplotlib import pyplot as plt
 
 def cartesian_to_config_space(x, y, l1, l2):
     if x > 0:
-        theta1, theta2 = cartesian_to_config_space_x_pos(x, y, l1, l2)
+        (theta1_pos, theta2_pos), (theta1_neg, theta2_neg) = cartesian_to_config_space_x_pos(x, y, l1, l2)
     else:
-        theta1, theta2 = cartesian_to_config_space_x_pos(-x, y, l1, l2)
-        theta1 = np.pi - theta1
-        theta2 = np.pi - theta2
-    return theta1, theta2
+        (theta1_pos, theta2_pos), (theta1_neg, theta2_neg) = cartesian_to_config_space_x_pos(-x, y, l1, l2)
+        theta1_pos = np.pi - theta1_pos
+        theta2_pos = np.pi - theta2_pos
+        theta1_neg = np.pi - theta1_neg
+        theta2_neg = np.pi - theta2_neg
+    alpha = np.arctan(y / x)
+    return (theta1_pos, theta2_pos), (theta1_neg, theta2_neg)
 
 def cartesian_to_config_space_x_pos(x, y, l1, l2):
     cos_theta2 = (x**2 + y**2 - l1**2 -l2**2) / (2 * l1 * l2)
-    theta2 = np.arccos(cos_theta2)
-    theta1 = np.arctan(y / x) - np.arctan((l2 * np.sin(theta2)) / (l1 + l2 * np.cos(theta2)))
-    return theta1, theta2
+    theta2_pos = np.arccos(cos_theta2)
+    theta2_neg = -theta2_pos
+    theta1_pos = np.arctan(y / x) - np.arctan((l2 * np.sin(theta2_pos)) / (l1 + l2 * np.cos(theta2_pos)))
+    theta1_neg = np.arctan(y / x) + np.arctan((l2 * np.sin(theta2_pos)) / (l1 + l2 * np.cos(theta2_pos)))
+    return (theta1_pos, theta2_pos), (theta1_neg, theta2_neg)
 
 def plot_arm(alpha, l1, l2, x, y):
     x1, y1 = l1 * np.cos(alpha), l1 * np.sin(alpha)
@@ -30,17 +35,18 @@ def plot_arm(alpha, l1, l2, x, y):
     plt.show()
 
 def check_arm_reach(x, y, l1, l2):
-    return (x + y)**2 >= l1 and (x + y)**2 >= l2
+    return (x**2 + y**2) <= (l1 + l2)**2
 def main():
-    x = -10
+    x = -2
     y = 5
     l1 = 10
     l2 = 10
     if not check_arm_reach(x, y, l1, l2):
         print("Point is outside arm length")
         return
-    theta1, theta2 = cartesian_to_config_space(x, y, l1, l2)
+    (theta1, theta2), (theta1_2, theta2_2) = cartesian_to_config_space(x, y, l1, l2)
     plot_arm(theta1, l1, l2, x, y)
+    plot_arm(theta1_2, l1, l2, x, y)
     print(f"{theta1=}, {theta2=}")
 
 if __name__ == "__main__":
